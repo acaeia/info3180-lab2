506279ad8b1f6a264c2509d91530023bda4ca6f5
Alexia Barrows <alexiabarrows@gmail.com>
2026-02-25 17:47:00 -0500

    first and final commit to lab 2 with completed instructions

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from app import app
 from flask import render_template, request, redirect, url_for, flash
-
+import datetime
 
 ###
 # Routing for your application.
@@ -17,10 +17,17 @@ def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
 
-
+@app.route('/profile')
+def profile():
+    date_joined = datetime.date(2026, 2, 25)
+    formatted_date = format_date_joined(date_joined)   
+    return render_template('profile.html', date_joined=formatted_date)
 ###
 # The functions below should be applicable to all Flask apps.
 ###
+
+def format_date_joined(date):
+    return date.strftime("%B, %Y")
 
 @app.route('/<file_name>.txt')
 def send_text_file(file_name):
